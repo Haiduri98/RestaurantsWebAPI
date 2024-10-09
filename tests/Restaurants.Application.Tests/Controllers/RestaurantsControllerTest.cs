7188a17d4866795545c524fd23d9822bcdb1701e
Haidar <hssl80@hotmail.com>
2024-10-10 00:06:47 +0200

    pushing the tests

---
+++ tests/Restaurants.Application.Tests/Controllers/RestaurantsControllerTest.cs
@@ -0,0 +1,146 @@
+﻿using System.Net.Http.Json;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using FluentAssertions;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Moq;
+using Restaurants.API.Controllers;
+using Restaurants.Application.Restaurants.Dtos;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Repositories;
+using Xunit;
+
+namespace Restaurants.Application.Tests.Controllers;
+
+// Integration Test
+
+[TestSubject(typeof(RestaurantsController))]
+public class RestaurantsControllerTest : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+    private readonly Mock<IRestaurantsRepository> _restaurantsRepositoryMock = new();
+
+    public RestaurantsControllerTest(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+                services.Replace(ServiceDescriptor.Scoped(typeof(IRestaurantsRepository),
+                    _ => _restaurantsRepositoryMock.Object));
+            });
+        });
+    }
+
+
+    [Fact]
+    public async Task GetAll_ForValidRequest_Returns200Ok()
+    {
+        // arrange
+        var client = _factory.CreateClient();
+
+        // act
+        var result = await client.GetAsync("/api/restaurants?pageSize=10&pageNumber=1");
+
+        // assert
+        result.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+    }
+
+
+    [Fact]
+    public async Task GetAll_ForValidRequest_Returns400BadRequest()
+    {
+        // arrange
+        var client = _factory.CreateClient();
+
+        // act
+        var result = await client.GetAsync("/api/restaurants");
+
+        // assert
+        result.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+    }
+
+
+    [Fact]
+    public async Task GetById_ForNonExistingId_ShouldReturn404NotFound()
+    {
+        // arrange
+        var id = 1123;
+        // this code makes the test independent of the actual db and its records
+        // therefore when using this line we return a null value
+        _restaurantsRepositoryMock.Setup(m => m.GetByIdAsync(id)).ReturnsAsync((Restaurant)null);
+        var client = _factory.CreateClient();
+
+        // act
+        var response = await client.GetAsync($"/api/restaurants/{id}");
+
+        // assert
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task GetById_ForExistingId_ShouldReturn200Ok()
+    {
+        // arrange
+        var id = 99;
+
+        var restaurant = new Restaurant()
+        {
+            Id = id,
+            Name = "Test Restaurant",
+            Description = "Test Restaurant",
+        };
+
+
+        _restaurantsRepositoryMock.Setup(m => m.GetByIdAsync(id)).ReturnsAsync(restaurant);
+        var client = _factory.CreateClient();
+
+        // act
+        var response = await client.GetAsync($"/api/restaurants/{id}");
+        var restaurantDto = await response.Content.ReadFromJsonAsync<RestaurantDto>();
+        // assert
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        restaurantDto.Name.Should().Be(restaurant.Name);
+        restaurantDto.Description.Should().Be(restaurant.Description);
+    }
+
+
+}
+
+
+// to bypass the authorization regarding testing
+internal class FakePolicyEvaluator : IPolicyEvaluator
+{
+    public Task<AuthenticateResult> AuthenticateAsync(AuthorizationPolicy policy, HttpContext context)
+    {
+        var claimsPrincipal = new ClaimsPrincipal();
+        claimsPrincipal.AddIdentity(new ClaimsIdentity(new []
+        {
+            new Claim(ClaimTypes.NameIdentifier, "1"),
+            new Claim(ClaimTypes.Role, "Admin"),
+        }));
+
+        var ticket = new AuthenticationTicket(claimsPrincipal, "Test");
+        var result = AuthenticateResult.Success(ticket);
+        return Task.FromResult(result);
+    }
+
+
+    public Task<PolicyAuthorizationResult> AuthorizeAsync(AuthorizationPolicy policy,
+        AuthenticateResult authenticationResult, HttpContext context,
+        object resource)
+    {
+        var result = PolicyAuthorizationResult.Success();
+        return Task.FromResult(result);
+    }
+}
---
+++ tests/Restaurants.Application.Tests/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandValidatorTest.cs
@@ -0,0 +1,103 @@
+﻿using FluentValidation.TestHelper;
+using JetBrains.Annotations;
+using Restaurants.Application.Restaurants.Commands.CreateRestaurant;
+using Xunit;
+
+namespace Restaurants.Application.Tests.Restaurants.Commands.CreateRestaurant;
+
+[TestSubject(typeof(CreateRestaurantCommandValidator))]
+public class CreateRestaurantCommandValidatorTest
+{
+    [Fact]
+    public void Validator_ForValidCommand_ShouldNotHaveValidationErrors()
+    {
+        // arrange
+        var command = new CreateRestaurantCommand()
+        {
+            Name = "Test",
+            Category = "Italian",
+            ContactEmail = "test@test.com",
+            PostalCode = "12-345",
+            Description = "Test description",
+        };
+
+        var validator = new CreateRestaurantCommandValidator();
+
+        // act
+        var result = validator.TestValidate(command);
+
+        // assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact]
+    public void Validator_ForInValidCommand_ShouldNHaveValidationErrors()
+    {
+        // arrange
+        var command = new CreateRestaurantCommand()
+        {
+            Name = "Te",
+            Category = "Ita",
+            ContactEmail = "@test.com",
+            PostalCode = "12345",
+            Description = "",
+        };
+
+        var validator = new CreateRestaurantCommandValidator();
+
+        // act
+        var result = validator.TestValidate(command);
+
+        // assert
+        result.ShouldHaveValidationErrorFor(c => c.Name);
+        result.ShouldHaveValidationErrorFor(c => c.Category);
+        result.ShouldHaveValidationErrorFor(c => c.ContactEmail);
+        result.ShouldHaveValidationErrorFor(c => c.PostalCode);
+        result.ShouldHaveValidationErrorFor(c => c.Description);
+    }
+
+    [Theory()]
+    [InlineData("Italian")]
+    [InlineData("Mexican")]
+    [InlineData("Japanese")]
+    [InlineData("American")]
+    [InlineData("Indian")]
+    public void Validator_ForValidCategory_ShouldNotHaveValidationErrorsForCategoryProperty(string category)
+    {
+        // arrange
+        var command = new CreateRestaurantCommand()
+        {
+            Category = category
+        };
+
+        var validator = new CreateRestaurantCommandValidator();
+
+        // act
+        var result = validator.TestValidate(command);
+
+        // assert
+        result.ShouldNotHaveValidationErrorFor(c => c.Category);
+    }
+
+    [Theory()]
+    [InlineData("10220")]
+    [InlineData("102-20")]
+    [InlineData("10 200")]
+    [InlineData("10-2 20")]
+    public void Validator_ForInValidPostalCode_ShouldHaveValidationErrorForPostalCodeProperty(string postalCode)
+    {
+        // arrange
+        var command = new CreateRestaurantCommand()
+        {
+            PostalCode = postalCode
+        };
+
+        var validator = new CreateRestaurantCommandValidator();
+
+        // act
+        var result = validator.TestValidate(command);
+
+        // assert
+        result.ShouldHaveValidationErrorFor(c => c.PostalCode);
+    }
+}
